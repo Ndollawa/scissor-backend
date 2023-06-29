1f48db1f286103e838e131a465c301e0175ca3a2
ndollawa <ndollawa@yahoo.com>
2023-06-29 08:53:09 +0100

    added db connection

--- src/app/Routes/api/shortenURL.ts
+++ src/app/Routes/api/shortenURL.ts
@@ -4,14 +4,14 @@ import ROLES_LIST  from '../../../config/roleList';
 import verifyRoles  from '../../Http/Middleware/verifyRoles';
 import URLShortenerController from '../../Http/Controllers/ShortenURLController';
 import verifyJWT from '../../Http/Middleware/verifyJWT';
-
+import shortenURLLimiter from '../../Http/Middleware/shortenURLLimiter';
 
 
 
 
 router.route('/')
 .get((req:Request, res:Response, next:NextFunction) => URLShortenerController.list(req,res,next))
-.post(verifyJWT,verifyRoles(ROLES_LIST.ADMIN,ROLES_LIST.DEV,ROLES_LIST.USER,ROLES_LIST.STAFF),(req:Request, res:Response, next:NextFunction) => URLShortenerController.create(req,res))
+.post(shortenURLLimiter,verifyJWT,verifyRoles(ROLES_LIST.ADMIN,ROLES_LIST.DEV,ROLES_LIST.USER,ROLES_LIST.STAFF),(req:Request, res:Response, next:NextFunction) => URLShortenerController.create(req,res))
 .patch(verifyJWT,verifyRoles(ROLES_LIST.ADMIN,ROLES_LIST.DEV,ROLES_LIST.USER,ROLES_LIST.STAFF),(req:Request, res:Response, next:NextFunction) => URLShortenerController.update(req,res))
 .put(verifyJWT,verifyRoles(ROLES_LIST.ADMIN,ROLES_LIST.DEV,ROLES_LIST.USER,ROLES_LIST.STAFF),(req:Request, res:Response, next:NextFunction) => URLShortenerController.update(req,res))
 .delete(verifyJWT,verifyRoles(ROLES_LIST.ADMIN,ROLES_LIST.DEV,ROLES_LIST.USER,ROLES_LIST.STAFF),(req:Request, res:Response, next:NextFunction) => URLShortenerController.delete(req,res))
