c61a24ad2cc80edda1a9bfac5da11091ff553608
Ndubuisi O <ndollawa@yahoo.com>
2024-06-14 17:07:42 +0100

    Update allowedOrigins.ts

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -3,6 +3,7 @@ const allowedOrigins:string[] = [
     'http://127.0.0.1:3000',
     'http://localhost:3500',
     'http://localhost:3000',
+    'https://localhost:3000',
     'https://scissor-frontend-f60y.onrender.com',
     'https://scissors-psi.vercel.app'
 ];
